38b4d37df2c8cb3fc79ee04f78fc2a759f4ba9cb
adilahmed845 <adilahmed845@gmail.com>
2023-09-20 14:10:47 +0530

    updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://adilahmed845:vqTUfzFkB3ld0MGD@cluster1.em1qvrw.mongodb.net/node-auth';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
