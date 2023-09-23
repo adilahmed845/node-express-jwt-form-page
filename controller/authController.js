bdb7465474a6227159692058e870341c828bcaf0
adilahmed845 <adilahmed845@gmail.com>
2023-09-23 15:40:15 +0530

    post login updated

--- controller/authController.js
+++ controller/authController.js
@@ -5,6 +5,16 @@ const jwt = require('jsonwebtoken');
 const handleError = (err) =>{
     console.log(err.message,err.code);
     let errors = {email:"",password:""};
+
+    //incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = "email is not registered"
+    }
+
+    //incorrect password
+    if(err.message === 'incorrect password'){
+        errors.password = "wrong password"
+    }
     
     //duplicate error 
     if(err.code === 11000){
@@ -19,9 +29,9 @@ const handleError = (err) =>{
             //console.log(error.properties.path)
             errors[properties.path] = properties.message
         });
-
-        return errors;
     }
+
+    return errors;
 } 
 
 const maxAge = 3*24*60*60;
@@ -58,11 +68,15 @@ module.exports.postSignup = async (req,res) =>{
 module.exports.postLogin= async (req,res) =>{
     const {email,password} = req.body;
     try{
-        const user = await User.login({email,password});
-        res.status(200).json({user: user._id});
+        const user = await User.login(email,password);
+        const token = createToken(user._id)
+        res.cookie('jwt',token,{httpOnly:true,maxAge:maxAge*1000});
+        res.status(201).json({user: user._id});
 
     }
     catch(err){
-        res.status(400).json({});
+        const errors = handleError(err);
+        console.log(errors);
+        res.status(400).json({errors});
     }
 }
