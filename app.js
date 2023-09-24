72fa76bff5442ddcb38d877b0d41e66e42a77c32
adilahmed845 <adilahmed845@gmail.com>
2023-09-24 09:31:13 +0530

    applied middleware to smoothies route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const authRoutes = require("./routes/authRoute");
+const {requireAuth} = require('./middleware/authMiddleware');
 const cookieParser = require('cookie-parser');
 
 // middleware
@@ -15,33 +16,16 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-
+const dbURI = "dbURL";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth,(req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-//cookies
-// app.get('/set-cookies',(req,res)=>{
-//   //res.setHeader("Set-Cookie", 'newUser=true');
 
-//   //cookie method comes with cookieParser middleware
-//   res.cookie('isEmployee',true,{maxAge:1000*60*60*24, httpOnly:true})
-//   res.cookie("todayUser",false);
-
-//   res.send("cookie is updated ")
-// })
-
-// app.get('/read-cookies',(req,res)=>{
-//   const cookies = req.cookies;
-  
-//   console.log(cookies);
-
-//   res.json(cookies)
-// })
 
 
